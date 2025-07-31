ff901da782bb7eb56223e5682109f8e8b579849f
zubairkachi <kachizubairhussain0@gmail.com>
2025-07-31 13:17:34 +0530

    Add files via upload

---
+++ Even or Odd.py
@@ -0,0 +1,5 @@
+num=int(input("Enter a number to check it is even or odd: "))
+if num % 2 == 0:
+    print(num,"is a even number")
+else:
+    print(num,"it is odd number")
---
+++ Sum of Integers.py
@@ -0,0 +1,4 @@
+num=0
+for n in range(1,51):
+    num=num+n
+print("The sum of number from 1 to 50 is: ",num)
